a968a407065cdffdf9fdf0b4a4e8375b42c7745a
dd <dordovskydmitry@gmail.com>
2015-10-01 21:36:32 +0200

    Refactor associations.

--- lib/tiny_orm/association/base.rb
+++ lib/tiny_orm/association/base.rb
@@ -5,7 +5,7 @@ module TinyORM
 
       def initialize(owner, target, options = {})
         @owner = owner
-        @target = options[:class_name] || target.to_s.split('_').each{|s| s.extend(TinyORM::PluralSingularString) }.map(&:singularize).map(&:capitalize).join
+        @target = options[:class_name] || camelize(target)
         @internal_key = options[:internal_key]
         @external_key = options[:external_key]
       end
@@ -16,6 +16,12 @@ module TinyORM
 
       def target
         Object.const_get(@target)
+      end
+
+      private
+
+      def camelize(str)
+        str.to_s.split('_').each{|s| s.extend(TinyORM::PluralSingularString) }.map(&:singularize).map(&:capitalize).join
       end
     end
   end
--- lib/tiny_orm/association/has_many.rb
+++ lib/tiny_orm/association/has_many.rb
@@ -3,7 +3,6 @@ module TinyORM
     class HasMany < Base
       def initialize(owner, target, options = {})
         super
-        @target
         @through = options[:through]
         @internal_key ||= :id
         @external_key ||= (@through ? :id : :"#{owner.to_s.downcase}_id")
@@ -11,13 +10,19 @@ module TinyORM
 
       def path
         return super unless @through
-        through = @through.to_s.split('_').each{|s| s.extend(TinyORM::PluralSingularString) }.map(&:singularize).map(&:capitalize).join
-        through_model = Object.const_get(through)
+        [owner_to_ligament, ligament_to_target]
+      end
 
-        owner_to_middle = @owner.associations[@through] || HasMany.new(@owner, @through, internal_key: @internal_key)
-        internal_key = "#{@target.to_s.downcase}_id"
-        target_to_middle = through_model.associations[@target.to_s.downcase.to_sym] || HasMany.new(through_model, nil, class_name:  @target, internal_key: internal_key, external_key: @external_key)
-        [owner_to_middle, target_to_middle]
+      private
+
+      def owner_to_ligament
+        @owner.associations[@through] || HasMany.new(@owner, @through, internal_key: @internal_key)
+      end
+
+      def ligament_to_target
+        through_model = Object.const_get(camelize(@through))
+        through_model.associations[@target.to_s.downcase.to_sym] ||
+            HasMany.new(through_model, nil, class_name: @target, internal_key: "#{@target.to_s.downcase}_id", external_key: @external_key)
       end
     end
   end
