2ed8ea15dd4ee61f7ef6798d0ddfdce716ea8d7e
dd <dordovskydmitry@gmail.com>
2015-10-01 22:16:36 +0200

    Refactor Query::Join.

--- lib/tiny_orm/query/join.rb
+++ lib/tiny_orm/query/join.rb
@@ -17,37 +17,35 @@ module TinyORM
       private
 
       def join_expression
-        @value.reduce([]) do |join_agg, arg|
-          join_agg << (arg.is_a?(Hash) ? expr_by_hash(arg) : expr_simple(arg))
-        end.join(' ')
+        @value.reduce([]) { |join_agg, arg| join_agg << complex_expression(@query.model, arg) }.join(' ')
       end
 
-      def expr_simple(arg)
-        association = @query.model.associations(arg)
+      def complex_expression(model, associations)
+        if associations.is_a?(Hash)
+          hash_expression(model, associations)
+        elsif associations.is_a?(Array)
+          array_expression(model, associations)
+        else
+          simple_expression(model, associations)
+        end
+      end
+
+      def hash_expression(model, associations)
+        associations.keys.reduce('') do |agg, key|
+          key_model = model.associations(key).target
+          agg << simple_expression(model, key) << ' ' << complex_expression(key_model, associations[key])
+        end
+      end
+
+      def array_expression(model, associations)
+        associations.reduce('') { |agg, key| agg << simple_expression(model, key) }
+      end
+
+      def simple_expression(k, v)
+        association = k.associations(v)
         association.path.map do |assoc|
           owner, target = assoc.owner, assoc.target
           "#{@type.to_s.upcase} JOIN #{target.table_name} ON #{owner.table_name}.#{assoc.internal_key} = #{target.table_name}.#{assoc.external_key}"
-        end.join(' ')
-      end
-
-      #TODO Implement  different relations (not only many-to-many) and deeper case
-      def expr_by_hash(arg)
-        arg.map do |k, v|
-          association = @query.model.associations(k)
-          association2 = association.target.associations(v)
-
-          s1 = association.path.map do |assoc|
-            owner, target = assoc.owner, assoc.target
-            "#{@type.to_s.upcase} JOIN #{target.table_name} ON #{owner.table_name}.#{assoc.internal_key} = #{target.table_name}.#{assoc.external_key}"
-          end.join(' ')
-
-          s2 = association2.path.map do |assoc|
-            owner, target = assoc.owner, assoc.target
-            "#{@type.to_s.upcase} JOIN #{target.table_name} ON #{owner.table_name}.#{assoc.internal_key} = #{target.table_name}.#{assoc.external_key}"
-          end.join(' ')
-
-          s1 + ' ' + s2
-
         end.join(' ')
       end
     end
