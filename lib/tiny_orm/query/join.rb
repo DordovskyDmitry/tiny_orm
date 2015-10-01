a3ccaa8a88f6dc637aaded6c5d75fd18d6c31105
dd <dordovskydmitry@gmail.com>
2015-10-01 20:35:46 +0200

    Rough Query::Join implementaton: build join by associations.

--- lib/tiny_orm/query/join.rb
+++ lib/tiny_orm/query/join.rb
@@ -1,8 +1,10 @@
 module TinyORM
   module Query
     class Join < Base
+      attr_reader :type
+
       def set(*value)
-        @value = *value
+        @type, @value = %w(inner left right).include?(value.first.to_s) ? [value.first, value.drop(1)] : [:inner, value]
         @query.join << self
         self
       end
@@ -20,15 +22,32 @@ module TinyORM
         end.join(' ')
       end
 
-      #TODO Implement different relations
       def expr_simple(arg)
-        "INNER JOIN #{arg} ON #{@query.table_name}.id = #{arg}.#{@query.table_name.singularize}_id"
+        association = @query.model.associations(arg)
+        association.path.map do |assoc|
+          owner, target = assoc.owner, assoc.target
+          "#{@type.to_s.upcase} JOIN #{target.table_name} ON #{owner.table_name}.#{assoc.internal_key} = #{target.table_name}.#{assoc.external_key}"
+        end.join(' ')
       end
 
       #TODO Implement  different relations (not only many-to-many) and deeper case
       def expr_by_hash(arg)
         arg.map do |k, v|
-          "INNER JOIN #{k} ON #{@query.table_name}.id = #{k}.#{@query.table_name.singularize}_id INNER JOIN #{v} ON #{v}.id = #{k}.#{v.to_s.singularize}_id"
+          association = @query.model.associations(k)
+          association2 = association.target.associations(v)
+
+          s1 = association.path.map do |assoc|
+            owner, target = assoc.owner, assoc.target
+            "#{@type.to_s.upcase} JOIN #{target.table_name} ON #{owner.table_name}.#{assoc.internal_key} = #{target.table_name}.#{assoc.external_key}"
+          end.join(' ')
+
+          s2 = association2.path.map do |assoc|
+            owner, target = assoc.owner, assoc.target
+            "#{@type.to_s.upcase} JOIN #{target.table_name} ON #{owner.table_name}.#{assoc.internal_key} = #{target.table_name}.#{assoc.external_key}"
+          end.join(' ')
+
+          s1 + ' ' + s2
+
         end.join(' ')
       end
     end
--- spec/query/join_spec.rb
+++ spec/query/join_spec.rb
@@ -1,13 +1,47 @@
-require_relative '../../spec/spec_helper'
+require_relative '../spec_helper'
+
+class Profile < TinyORM::Base
+end
+
+class Book < TinyORM::Base
+end
+
+class Photo < TinyORM::Base
+  self.table_name = 'photos'
+  belongs_to :profile
+end
+
+class AuthorBook < TinyORM::Base
+  self.table_name = 'author_books'
+  has_many :books
+end
+
+class Author < TinyORM::Base
+  self.table_name = 'authors'
+  has_one :profile
+  has_many :books, through: :author_books
+end
+
+class Profile
+  self.table_name = 'profiles'
+  belongs_to :author
+  has_many :photos
+end
+
+class Book
+  self.table_name = 'books'
+  has_many :author_books
+  has_many :authors, through: :author_books
+end
 
 describe TinyORM::Query::Join do
 
-  let(:join) { described_class.new(TinyORM::Query::Container.new(Model.new('users'))) }
+  let(:join) { described_class.new(TinyORM::Query::Container.new(Author)) }
 
-  it { expect(join.set('LEFT JOIN orders ON users.id = orders.user_id').compile!).to eq('LEFT JOIN orders ON users.id = orders.user_id') }
-  it { expect(join.set(:orders).compile!).to eq('INNER JOIN orders ON users.id = orders.user_id') }
-  it 'many-to-many' do
-    expect(join.set(user_addresses: :addresses).compile!)
-           .to eq('INNER JOIN user_addresses ON users.id = user_addresses.user_id INNER JOIN addresses ON addresses.id = user_addresses.address_id')
-  end
+  it { expect(join.set('LEFT JOIN orders ON authors.id = orders.author_id').compile!).to eq('LEFT JOIN orders ON authors.id = orders.author_id') }
+  it { expect(join.set(:inner, :profile).compile!).to eq('INNER JOIN profiles ON authors.id = profiles.author_id') }
+  it { expect(join.set(:left, :profile).compile!).to eq('LEFT JOIN profiles ON authors.id = profiles.author_id') }
+  it { expect(join.set(:books).compile!).to eq('INNER JOIN author_books ON authors.id = author_books.author_id INNER JOIN books ON author_books.book_id = books.id')}
+
+  it { expect(join.set(profile: :photos).compile!).to eq('INNER JOIN profiles ON authors.id = profiles.author_id INNER JOIN photos ON profiles.id = photos.profile_id')}
 end
